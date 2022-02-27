b3e4fb94c4685bce21cfb73b283471ac167ff13f
Bhupesh Rajpurohit <bhupeshpurohit494@gmail.com>
2022-02-27 19:03:01 +0530

    first commit on nodeBasics branch

---
+++ modules.js
@@ -0,0 +1,7 @@
+const { peoples, ages } = require("./people");
+const os = require("os");
+
+console.log(peoples);
+console.log(ages);
+
+console.log(os.platform(), os.homedir());
---
+++ people.js
@@ -0,0 +1,7 @@
+const peoples = ["yoshi", "mario", "luigi", "peach"];
+const ages = [32, 33, 34, 35];
+
+module.exports = {
+  peoples,
+  ages,
+};
