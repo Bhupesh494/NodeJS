81cc33a2ce5650dc0dc35a0682b793e3e7357dd8
Bhupesh Rajpurohit <bhupeshpurohit494@gmail.com>
2022-02-28 16:45:51 +0530

    new branch commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Request Made");
+  console.log(req);
 });
 server.listen(3000, "localhost", () => {
   console.log("server is running on port 3000");
