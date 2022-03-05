dff7caf48c525a91031dc2952c5a8816e6bf361e
Bhupesh Rajpurohit <bhupeshpurohit494@gmail.com>
2022-03-05 20:23:12 +0530

    done with npm lesson

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
   res.setHeader("Content-Type", "text/html");
+
+  // * lodash
+  const num = _.random(0, 20);
+  console.log(num);
+
+  // !lodash method once will allow to run this function once only
+  const greet = _.once(() => {
+    console.log("hrllo world");
+  });
+
+  greet();
+  greet();
+  greet();
+  greet();
 
   //   if (req.url === "/") {
   //     fs.readFile("./views/index.html", (err, data) => {
